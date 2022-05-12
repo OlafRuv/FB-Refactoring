f2545952cc17948442a04b0d51ed567bcc03c6eb
Olaf <olaf.ruvag@gmail.com>
2022-05-12 15:47:37 -0500

    ExplorerController aniadimos nuevo featureFizzbuzz  y pruebas del mismo

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,18 +1,29 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Unitary test for explorer controller", () => {
-    test("1) Explorer service filter by mission ussage", () => {
+    test("1) Explorer Controller filter by mission ussage", () => {
         const result = ExplorerController.getExplorersbyMission("node");
         expect(result).not.toBeUndefined();
     });
     
-    test("2) Explorer service get ammount of explorers by mission ussage", () => {
+    test("2) Explorer Controller get ammount of explorers by mission ussage", () => {
         const result = ExplorerController.getExplorersAmountbyMission("node");
         expect(result).toBe(10);
     });
 
-    test("3) Explorer service get explorer username by mission", () => {
+    test("3) Explorer Controller get explorer username by mission", () => {
         const result = ExplorerController.getExplorersUsernamebyMission("node");
         expect(result).not.toBeUndefined();
     });
+
+    test("4) Explorer Controller fizzbuzz validation number", () => {
+        const res1 = ExplorerController.fizzbuzzValidationNumber(3)
+        const res2 = ExplorerController.fizzbuzzValidationNumber(5)
+        const res3 = ExplorerController.fizzbuzzValidationNumber(15)
+        const res4 = ExplorerController.fizzbuzzValidationNumber(1)
+        expect(res1).toBe("FIZZ")
+        expect(res2).toBe("BUZZ")
+        expect(res3).toBe("FIZZBUZZ")
+        expect(res4).toBe(1)
+    })
 });
